88ae67814b7bf8cbd7386f663483a117b97e15b4
unknown <ryanwiggins52@gmail.com>
2019-09-15 13:22:17 -0400

    display user added

--- app.js
+++ app.js
@@ -1,27 +1,29 @@
 // init github
 const github = new Github()
 
+// init UI class
+const ui = new UI()
+
 // search input
 
 const searchUser = document.getElementById('searchUser')
 
 // Search input event listener
 
-searchUser.addEventListener('keyup', (e) => {
+searchUser.addEventListener('keyup', e => {
   // Get input text
   const userText = e.target.value
 
   if (userText !== '') {
     // Make HTTP call
-    github.getUser(userText)
-      .then(data => {
-        if (data.profile.message === 'Not Found') {
-          // Show Alert
-
-        } else {
-          // Show Profile
-        }
-      })
+    github.getUser(userText).then(data => {
+      if (data.profile.message === 'Not Found') {
+        // Show Alert
+      } else {
+        // Show Profile
+        ui.showProfile(data.profile)
+      }
+    })
   } else {
     // clear profile
   }

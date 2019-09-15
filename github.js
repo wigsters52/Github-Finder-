2ec08fbe6bd67b6c09fd861a091a0a4195108bc2
unknown <ryanwiggins52@gmail.com>
2019-09-15 14:21:18 -0400

    Added repo display

--- github.js
+++ github.js
@@ -1,18 +1,26 @@
 class Github {
-  constructor () {
+  constructor() {
     this.client_id = 'f402faabec79f2508cd5'
     this.client_secret = 'ae5cffc7345291a2f5e5e10eada4623a38f267d2'
+    this.repos_count = 5
+    this.repos_sort = 'created: asc'
   }
 
-  async getUser (user) {
+  async getUser(user) {
     const profileResponse = await fetch(
       `https://api.github.com/users/${user}?client_id=${this.client_id}$client_secret=${this.client_secret}`
     )
 
+    const repoResponse = await fetch(
+      `https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sory=${this.repos_sort}&client_id=${this.client_id}$client_secret=${this.client_secret}`
+    )
+
     const profile = await profileResponse.json()
+    const repos = await repoResponse.json()
 
     return {
-      profile
+      profile,
+      repos
     }
   }
 }

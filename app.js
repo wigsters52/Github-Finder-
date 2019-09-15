a93de1451d0cb62fe0e15580f4a2e4a5f97b97e4
unknown <ryanwiggins52@gmail.com>
2019-09-15 13:59:20 -0400

    show alert message added

--- app.js
+++ app.js
@@ -1,30 +1,29 @@
-// init github
+// Init Github
 const github = new Github()
-
-// init UI class
+// Init UI
 const ui = new UI()
 
-// search input
-
+// Search input
 const searchUser = document.getElementById('searchUser')
 
 // Search input event listener
-
 searchUser.addEventListener('keyup', e => {
   // Get input text
   const userText = e.target.value
 
   if (userText !== '') {
-    // Make HTTP call
+    // Make http call
     github.getUser(userText).then(data => {
       if (data.profile.message === 'Not Found') {
-        // Show Alert
+        // Show alert
+        ui.showAlert('User not found', 'alert alert-danger')
       } else {
-        // Show Profile
+        // Show profile
         ui.showProfile(data.profile)
       }
     })
   } else {
-    // clear profile
+    // Clear profile
+    ui.clearProfile()
   }
 })
--- github.js
+++ github.js
@@ -5,7 +5,9 @@ class Github {
   }
 
   async getUser (user) {
-    const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}$client_secret=${this.client_secret}`)
+    const profileResponse = await fetch(
+      `https://api.github.com/users/${user}?client_id=${this.client_id}$client_secret=${this.client_secret}`
+    )
 
     const profile = await profileResponse.json()
 
